afb4b68d6d0acf259d1bcb6a6e8c8bdeb3659621
YsolineG <ysoline.ganster@gmail.com>
2021-06-08 11:05:57 +0200

    feat: add class Database

---
+++ Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database
+{
+    const DB_HOST = 'mysql:host=localhost;dbname=blog_project';
+    const DB_USER = 'root';
+
+    public function getConnection()
+    {
+        try {
+            $connection = new PDO(self::DB_HOST, self::DB_USER);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return 'Connexion OK';
+        } 
+        catch (Exception $errorConnection)
+        {
+            die ('Erreur de connexion:'.$errorConnection->getMessage());
+        }
+    }
+}
