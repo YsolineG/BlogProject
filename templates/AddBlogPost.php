6e4d2976f6d823f753839005ca04cd2ba9ebba00
YsolineG <ysoline.ganster@gmail.com>
2021-08-02 10:43:52 +0200

    refactor: call formBlogPost.php

--- templates/AddBlogPost.php
+++ templates/AddBlogPost.php
@@ -1,13 +1,7 @@
-<?php
-$route = isset($post) && $post->get('id') ? 'editBlogPost&idBlogPost='.$post->get('blog_post_id') : 'addBlogPost';
-$submit = $route === 'addBlogPost' ? 'Envoyer' : 'Mettre à jour';
-?>
-<form method="post" action="../public/index.php?route=<?= $route ?>">
-    <label for="title">Titre</label><br>
-    <input type="text" id="title" name="title" value="<?= isset($post) ? htmlspecialchars($post->get('title')): '' ?>"><br>
-    <?= isset($errors['title']) ? $errors['title'] : ''; ?>
-    <label for="content">Contenu</label><br>
-    <textarea id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get('content')): '' ?></textarea><br>
-    <?= isset($errors['content']) ? $errors['content'] : ''; ?>
-    <input type="submit" value="<?= $submit; ?>" id="submit" name="submit">
-</form>
+<?php $this->title = 'Nouvel article'?>
+<h1>Mon blog</h1>
+<p>En construction</p>
+<div>
+    <?php include('formBlogPost.php');?>
+    <a href="../public/index.php">Retour à l'accueil</a>
+</div>
