d8dd15174a6a3d5dbe6ad6ac5ff7c9ff14f356b3
YsolineG <ysoline.ganster@gmail.com>
2021-09-20 17:54:30 +0200

    refactor: change the error message

--- src/constraint/Constraint.php
+++ src/constraint/Constraint.php
@@ -7,19 +7,19 @@ class Constraint
     public function notBlank($name, $value)
     {
         if(empty($value)) {
-            return 'Le champ '.$name.' saisi est vide';
+            return 'Le champ de saisi est vide';
         }
     }
     public function minLength($name, $value, $minSize)
     {
         if(strlen($value) < $minSize) {
-            return 'Le champ '.$name.' doit contenir au moins '.$minSize.' caractères';
+            return 'Le champ doit contenir au moins '.$minSize.' caractères';
         }
     }
     public function maxLength($name, $value, $maxSize)
     {
         if(strlen($value) > $maxSize) {
-            return 'Le champ '.$name.' doit contenir au maximum '.$maxSize.' caractères';
+            return 'Le champ doit contenir au maximum '.$maxSize.' caractères';
         }
     }
 }
